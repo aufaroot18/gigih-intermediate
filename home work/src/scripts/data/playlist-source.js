b10a6b4e1872130a0a18191769523d19bf5de864
Aufa Billah <aufaroot18@gmail.com>
2021-07-09 00:24:17 +0700

    add error handling in async await

--- home work/src/scripts/data/playlist-source.js
+++ home work/src/scripts/data/playlist-source.js
@@ -1,9 +1,14 @@
 import API_ENDPOIT from '../globals/api-endpoint.js';
 
 const getPlaylist = async () => {
-  const responses = await fetch(API_ENDPOIT.playlist);
-  const playlist = await responses.json();
-  return playlist;
+  try {
+    const responses = await fetch(API_ENDPOIT.playlist);
+    const playlist = await responses.json();
+    return playlist;
+  }
+  catch(err) {
+    throw new Error(err);
+  }
 }
 
 const playlist = getPlaylist();
