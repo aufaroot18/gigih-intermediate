49a2d4e8f6378882779c582741ff037a2b614b2b
Aufa Billah <aufaroot18@gmail.com>
2021-07-08 17:20:57 +0700

    Add config and api-endpoint file

---
+++ home work/src/scripts/globals/api-endpoint.js
@@ -0,0 +1,7 @@
+import CONFIG from './config.js';
+
+const API_ENDPOINT = {
+  playlist: `${CONFIG.BASE_URL}/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json`,
+};
+
+export default API_ENDPOINT;
---
+++ home work/src/scripts/globals/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  key: '',
+  BASE_URL: 'https://gist.githubusercontent.com/',
+};
+
+export default CONFIG;
