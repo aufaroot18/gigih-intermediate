c5a8f5f6db84c5b47d6de4c6037ced2d9be17c6e
Aufa Billah <aufaroot18@gmail.com>
2021-07-08 17:22:01 +0700

    Render view home

--- home work/src/scripts/main.js
+++ home work/src/scripts/main.js
@@ -1,18 +1,5 @@
-console.log('hello');
+import home from './views/home.js';
 
-// grab dom
-const form = document.querySelector('.playlist-form');
-
-// handle submit form
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const title = form.elements['title'].value;
-  const description = form.elements['description'].value;
-
-  alert(`
-    title: ${title}
-    description: ${description}
-  `);
-
-  form.reset();
+document.addEventListener('DOMContentLoaded', async () => {
+  home();
 });
